f57ef69aa136671cda6acc655f2bea33a0ede04a
reayaaquino <reaya.aquino@gmail.com>
2026-08-06 04:07:25 +0800

    Update cities_with_completed_trades.sql

--- datalemur/easy/cities_with_completed_trades.sql
+++ datalemur/easy/cities_with_completed_trades.sql
@@ -7,9 +7,9 @@ Problem Summary:
 Retrieve the top 3 cities with the highest number of completed orders in descending order.
 
 Key Takeaways:
+- Used table aliases (trades t, users u) to keep column prefixes clean and prevent ambiguous column reference errors
 - GROUP BY u.city collapses the dataset down to 1 row per city, then COUNT(*) aggregates the total orders
 - Must filter WHERE t.status = 'Completed' before grouping
-- Used table aliases (trades t, users u) to keep column prefixes clean and prevent ambiguous column reference errors
 */
 
 SELECT 
