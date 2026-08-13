5388dfc314a17023b540a65d07f20ad15f93f61a
reayaaquino <reaya.aquino@gmail.com>
2026-08-13 20:10:51 +0800

    Update app_clickthrough_rate.sql

--- datalemur/easy/app_clickthrough_rate.sql
+++ datalemur/easy/app_clickthrough_rate.sql
@@ -4,13 +4,13 @@
 
 /*
 Problem Summary:
-Calculated the Click-Through Rate (CTR) for each app in 2022 and rounded the final percentage to two decimal places.
+Calculated the Click-Through Rate (CTR) for each app in 2022 and rounded the final percentage to two decimal places
 
 Key Takeaways:
--Used CASE WHEN statements inside COUNT() to filter and tally impressions and clicks in the same step.
--Multiplied by 100.0 instead of 100 so SQL calculates the result as a decimal rather than rounding down to a whole integer.
--I originally filtered dates with EXTRACT(), but realized using standard < and >= comparison operators is better for performance because it lets SQL use indexes.
--I also realized I didn't strictly need a CTE here—I could have done this in a single SELECT statement using SUM(CASE WHEN ...)—but the CTE kept my logic readable.
+-Used CASE WHEN statements inside COUNT() to filter and tally impressions and clicks in the same step
+-Multiplied by 100.0 instead of 100 so SQL calculates the result as a decimal rather than rounding down to a whole integer
+-I originally filtered dates with EXTRACT(), but realized using standard < and >= comparison operators is better for performance because it lets SQL use indexes
+-I also realized I didn't strictly need a CTE here—I could have done this in a single SELECT statement using SUM(CASE WHEN ...)—but the CTE kept my logic readable
 */
 
 WITH event_cte AS (
